37c5cc163a3111ffb9a308e767394a1fb6731bf4
asriidev-gh <asriidev@gmail.com>
2020-12-14 19:42:14 +0800

    fix error

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  MONGODB:
+    "mongodb+srv://socmeddev:socmeddev@cluster0.d4x3k.gcp.mongodb.net/socmedia?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
